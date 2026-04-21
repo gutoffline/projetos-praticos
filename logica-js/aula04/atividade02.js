3b31e0f28ac7b9a429ad75cb3541d61ae1916ed0
Guto Xavier <gutoffline@gmail.com>
2026-04-20 22:16:32 -0300

    Add aula04 JS exercises and README
    Create logica-js package with a README and multiple aula04 examples demonstrating loops and array iteration. Added while, do-while and for loop examples (atividade01-03), array iteration exercises (atividade04-05), and exercises for odd numbers, generating interval numbers, and summing an interval (exercicio02-04).

---
+++ logica-js/aula04/atividade02.js
@@ -0,0 +1,15 @@
+// Laço Faça Equanto
+
+let i = 1
+do {
+    console.log("Botafogo Campeão")
+    i++
+} while (i<1);
+
+let contador = 0
+do{
+    console.log(contador)
+    contador++
+}while(contador<10)
+console.log("===>" + contador)
+
---
+++ logica-js/aula04/atividade04.js
@@ -0,0 +1,7 @@
+let frutas = ["🍇","🍓","🍊","🥑"]
+frutas.push("🍐")
+
+for(let i = 0; i<frutas.length;i++){
+    console.log(frutas[i])
+}
+
